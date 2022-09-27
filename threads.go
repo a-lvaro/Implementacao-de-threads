ee3c444e695bf677983dd9aad35c9d5a12ad5c12
a-lvaro <aaflneto@gmail.com>
2022-09-27 08:56:36 -0300

    tudo certo com a soma e a subtração

--- threads.go
+++ threads.go
@@ -1,29 +1,18 @@
 package main
 
-func splitMatrix(matrixA [][]int, matrixB [][]int, s chan<- [][]int) {
+func threadsMatrix(matrixA [][]int, matrixB [][]int, s chan<- [][]int, flag string) {
 	// cpu := runtime.NumCPU()
 	size := len(matrixA)
 
 	for i := 0; i < size; i++ {
-		// fmt.Println(matrixA[i : i+size/cpu])
-		// fmt.Printf("[ %d : %d ] \n", i, i+size/cpu)
-		// fmt.Println(matrixA[i : i+size/cpu])
 
-		s <- sumMatrix(matrixA[i:i+1], matrixB[i:i+1])
+		if flag == "SUM" {
+			s <- sumMatrix(matrixA[i:i+1], matrixB[i:i+1])
+		} else if flag == "SUB" {
+			s <- subMatrix(matrixA[i:i+1], matrixB[i:i+1])
+		}
+
 	}
 	close(s)
-
-}
-
-func teste(matrixA [][]int) {
-	// size := len(matrixA)
-
-	// for i := 0; i < size; i++ {
-	// 	// fmt.Println(matrixA[i : i+size/cpu])
-	// 	// fmt.Printf("[ %d : %d ] \n", i, i+size/cpu)
-	// 	// fmt.Println(matrixA[i : i+size/cpu])
-
-	// 	s <- sumMatrix(matrixA[i:i+size/cpu], matrixB[i:i+size/cpu])
-	// }
 
 }
